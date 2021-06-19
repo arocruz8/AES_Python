a6aea1e6a7f0eecf9d2dcaa3ffb13d04221a5883
arocruz8 <arocruz8@gmail.com>
2021-06-19 15:05:36 -0600

    first commit

---
+++ AesFinal.py
@@ -0,0 +1,37 @@
+##Algoritmo AES 
+
+##Librerias
+from Cryptodome.Cipher import AES
+from secrets import token_bytes
+
+##Llave de 16 bytes de tamaño
+llave = token_bytes(16)
+
+##Función que encripta
+def encriptar(msg):
+    cifrado = AES.new(llave, AES.MODE_EAX)
+    nonce = cifrado.nonce
+    ##se aplica el algoritmo de encriptacion
+    textocifrado, tag = cifrado.encrypt_and_digest(msg.encode('ascii'))
+    return nonce, textocifrado, tag
+
+##Función que desencriptado
+def desencriptar(nonce, textocifrado, tag):
+    cifrado  = AES.new(llave, AES.MODE_EAX, nonce=nonce)
+    ##aplica el algoritmo para desencriptar el texto cifrado
+    textInicial =  cifrado.decrypt(textocifrado)
+    try:
+        cifrado.verify(tag)
+        ##retorna el algoritmo desencriptado
+        return textInicial.decode('ascii')
+    except:
+        return False
+
+##Ejecución del código
+nonce, textocifrado, tag = encriptar(input('Ingrese el mensaje que desea encriptar: '))
+textInicial = desencriptar(nonce, textocifrado, tag)
+print(f'Texto cifrado: {textocifrado}')
+if not textInicial:
+    print('El mensaje fue corrupto')
+else:
+    print(f'Texto descencriptado: {textInicial}')
